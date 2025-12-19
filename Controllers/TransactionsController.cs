0441b2b2ebb95099e8df0d23a6525b718f56c9c7
Shin40411 <dtho961@gmail.com>
2025-12-19 22:08:26 +0700

    add claim userId for transaction

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -1,13 +1,16 @@
 ﻿using Expense_tracker_api.Application.DTOs.Transaction;
 using Expense_tracker_api.Application.Interfaces;
 using Expense_tracker_api.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Expense_tracker_api.Controllers
 {
     [ApiController]
     [Route("api/transactions")]
+    [Authorize]
     public class TransactionsController : ControllerBase
     {
         private readonly ITransactionService _transactionService;
@@ -20,9 +23,19 @@ namespace Expense_tracker_api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateTransactionDto dto)
         {
-            var userId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+            var userId = GetUserIdFromToken();
             await _transactionService.CreateAsync(dto, userId);
             return Ok();
+        }
+
+        private Guid GetUserIdFromToken()
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userIdClaim == null)
+                throw new UnauthorizedAccessException("UserId not found in token");
+
+            return Guid.Parse(userIdClaim.Value);
         }
     }
 }
